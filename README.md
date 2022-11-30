8c59a521daf568308487a24ad5738d5bb1e2a47a
renzo <renzo_or@hotmail.com>
2022-11-30 09:30:33 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Laravel Tenancy
+
+Foobar is a Python library for dealing with word pluralization.
+
+## Installation
+lorem
+
+
+```bash
+lorem
+```
+
+## Instalacion
+
+```Laravel
+git clone 'link projer github'
+
+composer install
+
+cp .env.example .env or copy .env.example .env
+
+php artisan key:generate
+
+php artisan migrate
+
+php artisan db:seed
+
+php artisan serve
+
+Go to link localhost:8000 OR 127.0.0.1:8000
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\RegisteredTenantController;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\ProfileController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// Route::get('/register-tenant', [RegisteredTenantController::class, 'create' ]);
+// Route::post('/register-tenant', [RegisteredTenantController::class, 'store' ]);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
