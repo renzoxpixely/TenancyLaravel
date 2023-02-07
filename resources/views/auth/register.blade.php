4eb5299bb41e27c84eae62d55ed4ba9d5a9435c1
jose <estrella.pancorbo@gmail.com>
2023-02-07 18:15:45 -0500

    cambio al registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -46,7 +46,7 @@
                 <!-- Validation Errors -->
                 <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-                <form method="POST" action="/register-tenant">
+                <form method="POST" action="{{ route('register') }}">
                     @csrf
 
                     <fieldset>
